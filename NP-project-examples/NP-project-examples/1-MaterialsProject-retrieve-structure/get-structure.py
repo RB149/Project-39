41b2c7b4c4afd6934d1e5acb911cd3c4bc3a9288
Rebecca B <qtl506@york.ac.uk>
2025-10-05 22:15:12 +0100

    Upload NP-project-examples file to teaching0
    Will run through examples and test code for Au.

---
+++ NP-project-examples/NP-project-examples/1-MaterialsProject-retrieve-structure/get-structure.py
@@ -0,0 +1,18 @@
+from mp_api.client import MPRester
+
+formula_to_retrieve="Au"
+
+#initialise interface to Materials Project (not need to include your USER_API_KEY from MP website in .pmgrc.yaml[pymqtgen])
+mpr = MPRester()
+
+#search MP for most stable material with given formula
+docs = mpr.materials.summary.search(
+        energy_above_hull=(0, 0), formula = formula_to_retrieve, fields=["material_id"]
+    )
+stable_mpids = [doc.material_id for doc in docs]
+
+#get corresponding structure
+structure = mpr.get_structure_by_material_id(stable_mpids[0])
+
+#write out structure as poscar (can visualise easily in VESTA https://jp-minerals.org/vesta/en/)
+structure.get_sorted_structure().to("structure.vasp", "poscar")
