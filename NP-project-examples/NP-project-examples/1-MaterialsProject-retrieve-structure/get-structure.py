ff1c2291f275e93ff9836e9429799114c47412cd
RL Baxter <qtl506@york.ac.uk>
2025-10-16 02:22:46 +0100

    API key still not working after initial advice. Will send mail to ask more.

--- NP-project-examples/NP-project-examples/1-MaterialsProject-retrieve-structure/get-structure.py
+++ NP-project-examples/NP-project-examples/1-MaterialsProject-retrieve-structure/get-structure.py
@@ -3,12 +3,13 @@ from pymatgen.ext.matproj import MPRester
 formula_to_retrieve="Au"
 
 #initialise interface to Materials Project (not need to include your USER_API_KEY from MP website in .pmgrc.yaml[pymqtgen])
-with MPRester(kIviblPxcYcl6OuJMH3NMmlD3LYfY5Ws) as mpr:
+with MPRester("kIviblPxcYcl6OuJMH3NMmlD3LYfY5Ws") as mpr:
+# ^you are the problem. i see you. you taunt me. sneaking around the edges of my nightmares there is your face api key kIviblPxcYc160uJMH3NMm1D3LYfY5Zs
 #mpr = MPRester(api_key=kIviblPxcYc160uJMH3NMm1D3LYfY5Zs)
 
 #search MP for most stable material with given formula
 	docs = mpr.materials.summary.search(
-	        energy_above_hull=(0, 0), formula = formula_to_retrieve, fields=[mp-81]
+	        energy_above_hull=(0, 0), formula = formula_to_retrieve, fields=["mp-81"]
 	    )
 	stable_mpids = [doc.material_id for doc in docs]
 
