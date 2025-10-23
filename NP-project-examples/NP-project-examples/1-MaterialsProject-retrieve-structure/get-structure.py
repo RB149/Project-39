2583a62cf2c8b7f88822e72d7cbf0315ae6b38ef
RL Baxter <qtl506@york.ac.uk>
2025-10-23 12:26:59 +0100

    files edited last friday. 1st folder's code works now.

--- NP-project-examples/NP-project-examples/1-MaterialsProject-retrieve-structure/get-structure.py
+++ NP-project-examples/NP-project-examples/1-MaterialsProject-retrieve-structure/get-structure.py
@@ -1,19 +1,20 @@
-from pymatgen.ext.matproj import MPRester
+from mp_api.client import MPRester
 
 formula_to_retrieve="Au"
 
 #initialise interface to Materials Project (not need to include your USER_API_KEY from MP website in .pmgrc.yaml[pymqtgen])
-with MPRester("kIviblPxcYcl6OuJMH3NMmlD3LYfY5Ws") as mpr:
+mpr = MPRester()
+#api key kIviblPxcYc160uJMH3NMm1D3LYfY5Zs
 #mpr = MPRester(api_key=kIviblPxcYc160uJMH3NMm1D3LYfY5Zs)
 
 #search MP for most stable material with given formula
-	docs = mpr.materials.summary.search(
-	        energy_above_hull=(0, 0), formula = formula_to_retrieve, fields=["mp-81"]
-	    )
-	stable_mpids = [doc.material_id for doc in docs]
+docs = mpr.materials.summary.search(
+        energy_above_hull=(0, 0), formula = formula_to_retrieve, fields=["material_id"]
+    )
+stable_mpids = [doc.material_id for doc in docs]
 
 #get corresponding structure
-	structure = mpr.get_structure_by_material_id(stable_mpids[0])
+structure = mpr.get_structure_by_material_id(stable_mpids[0])
 
 #write out structure as poscar (can visualise easily in VESTA https://jp-minerals.org/vesta/en/)
-	structure.get_sorted_structure().to("structure.vasp", "poscar")
+structure.get_sorted_structure().to("structure.vasp", "poscar")
