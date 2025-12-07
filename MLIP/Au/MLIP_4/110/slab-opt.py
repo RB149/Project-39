c25d6291a5cfb3ba6ef61ef69634bf901d361847
RL Baxter <qtl506@login2.viking2.yor.alces.network>
2025-12-07 20:37:16 +0000

    moving to zot

---
+++ MLIP/Au/MLIP_4/110/slab-opt.py
@@ -0,0 +1,18 @@
+from janus_core.calculations.geom_opt import GeomOpt
+from ase.io import read, write
+
+struct = read("./slab-(1, 1, 0).vasp")
+
+geom_opt = GeomOpt(
+    arch="mace_mp",
+    #model path leads to  MLIP models directory
+    model_path="/users/qtl506/scratch/Project-39/MLIP/mace_models/2023-12-03-mace-128-L1_epoch-199.model",
+    struct=struct,
+    fmax=0.001,
+    filter_kwargs={"constant_volume" : True}
+    )
+#note constant volume for slab opt
+geom_opt.run()
+
+write('slab-(1, 1, 0)-opt.vasp', geom_opt.struct, format="vasp")
+print("Optimized energy (eV):", struct.get_potential_energy())
