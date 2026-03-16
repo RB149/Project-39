b13723436fae0f0d24adae95c63d5993c550c8ca
RL Baxter <qtl506@zot.its.york.ac.uk>
2026-03-16 14:03:43 +0000

    Wulff + Finite partile optimisation for Au

--- MLIP/Au/6-Wulff-construction/wulff.py
+++ MLIP/Au/6-Wulff-construction/wulff.py
@@ -2,8 +2,8 @@ from wulffpack import SingleCrystal
 from ase.io import read, write
 
 prim = read("./opt.vasp")
-surface_energies = {(1, 1, 1): 0.6, (1, 0, 0): 0.8}
-particle = SingleCrystal(surface_energies,primitive_structure=prim,natoms=300)
+surface_energies = {(1, 1, 1): 0.67, (1, 0, 0): 0.87, (1, 1, 0): 0.88 }
+particle = SingleCrystal(surface_energies,primitive_structure=prim,natoms=20) #300
 particle.view()
 write('atoms.xyz', particle.atoms)
 
