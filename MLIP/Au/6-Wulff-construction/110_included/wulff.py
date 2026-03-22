b5587d5e797797ef61b7cadb8c957efa2b90e1e7
RL Baxter <qtl506@zot.its.york.ac.uk>
2026-03-22 15:17:11 +0000

    Wulff Cu and Fe, with volume issue for Fe model

--- MLIP/Au/6-Wulff-construction/110_included/wulff.py
+++ MLIP/Au/6-Wulff-construction/110_included/wulff.py
@@ -2,7 +2,8 @@ from wulffpack import SingleCrystal
 from ase.io import read, write
 
 prim = read("./opt.vasp")
-surface_energies = {(1, 1, 1): 4.1, (1, 1, 0): 3.8, (1, 0, 0): 2.0}
+surface_energies = {(1, 1, 1): 0.6, (1, 0, 0): 0.8, (1, 1, 0): 0.9} #Au. same result if including the 110 index or not.
+# move this file to Au folder.
 particle = SingleCrystal(surface_energies,primitive_structure=prim,natoms=300)
 particle.view()
 write('atoms.xyz', particle.atoms)
