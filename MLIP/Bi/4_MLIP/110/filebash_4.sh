dca1ba302bdc550305265e298adad49a86ae5766
RL Baxter <qtl506@zot.its.york.ac.uk>
2026-04-29 12:03:11 +0100

    cleanup of unsued material folders

--- MLIP/Bi/4_MLIP/110/filebash_4.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#querying where output files are moved to
-read -p "Where to put the files?: " d_name
-#moving output and source data files
-mv 'slab-(1, 1, 0).vasp' 'slab-(1, 1, 0)-opt.vasp' out* $d_name
