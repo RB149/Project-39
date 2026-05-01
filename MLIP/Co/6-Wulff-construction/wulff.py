e105299cc7aac0903e7e6d2e3b335d64e20601f9
RL Baxter <qtl506@zot.its.york.ac.uk>
2026-05-01 09:24:14 +0100

    Au finite particle 5 atoms generation, minor reorganisation of Co folder

--- MLIP/Co/6-Wulff-construction/wulff.py
+++ MLIP/Co/6-Wulff-construction/wulff.py
@@ -2,7 +2,7 @@ from wulffpack import SingleCrystal
 from ase.io import read, write
 
 prim = read("./opt.vasp")
-surface_energies = {(1, 1, 1): 2.4, (1, 1, 0): 2.0, (1, 0, 0): 2.0}
+surface_energies = {(1, 1, 1): 2.02, (1, 1, 0): 2.31, (1, 0, 0): 2.27}
 particle = SingleCrystal(surface_energies,primitive_structure=prim,natoms=100)
 particle.view()
 write('atoms.xyz', particle.atoms)
